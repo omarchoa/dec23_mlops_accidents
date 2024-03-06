9cd2907b5052651f37679434629a772691721634
Omar Choa <omar.choa@gmail.com>
2024-03-06 22:03:48 +0100

    Update userlogin.py
    - Added attribute `.user_data` as an optional argument in `.__init__()` definition
    - Initialized `.user_data` in `.__init__()`
    - Black Formatter applied automatic code formatting

--- src/data/userlogin.py
+++ src/data/userlogin.py
@@ -1,33 +1,35 @@
 import os
 import pickle
 
+
 class BddLogin:
-    def __init__(self):
+    def __init__(self, user_data=None):
+        self.user_data = user_data
         bdd_login_filename = "login_bdd"  # full path to the bdd filemane
         if os.path.isfile(bdd_login_filename):
-            with open(bdd_login_filename, 'rb') as bdd:
+            with open(bdd_login_filename, "rb") as bdd:
                 self.user_data = pickle.load(bdd)
         else:
             self.user_data = {
-                'alexandre_winger': 'user',
-                'fabrice_charraud': 'user',
-                'michaël_deroche': 'user',
-                'omar_choa': 'user',
-                'alban_thuet': 'admin',
+                "alexandre_winger": "user",
+                "fabrice_charraud": "user",
+                "michaël_deroche": "user",
+                "omar_choa": "user",
+                "alban_thuet": "admin",
             }
-            with open(bdd_login_filename, 'wb') as bdd:
+            with open(bdd_login_filename, "wb") as bdd:
                 pickle.dump(self.user_data, bdd)
 
     def is_(self, right, login, password):
-        key = f'{login}_{password}'
+        key = f"{login}_{password}"
         if key in self.user_data and self.user_data[key] == right:
             return True
         return False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bdd = BddLogin()
-    print(bdd.is_('user', 'tom', ''))  # should be False
-    print(bdd.is_('user', 'fabrice', ''))  # should be False
-    print(bdd.is_('user', 'alexandre', 'winger'))  # should be True
-    print(bdd.is_('admin', 'alban', 'thuet'))  # should be True
+    print(bdd.is_("user", "tom", ""))  # should be False
+    print(bdd.is_("user", "fabrice", ""))  # should be False
+    print(bdd.is_("user", "alexandre", "winger"))  # should be True
+    print(bdd.is_("admin", "alban", "thuet"))  # should be True
