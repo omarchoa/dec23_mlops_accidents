6d9313fb182a2afc9c4dc7db7d671e9155962e73
Omar Choa <omar.choa@gmail.com>
2024-03-06 22:34:36 +0100

    Update userlogin.py
    - Commented code

--- src/data/userlogin.py
+++ src/data/userlogin.py
@@ -3,13 +3,19 @@ import pickle
 
 
 class BddLogin:
-    def __init__(self, user_data=None):
-        self.user_data = user_data
+    def __init__(self, user_data=None):  # override special method .__init__()
+        self.user_data = user_data  # initialize .user_data
         bdd_login_filename = "login_bdd"  # full path to the bdd filemane
-        if os.path.isfile(bdd_login_filename):
-            with open(bdd_login_filename, "rb") as bdd:
-                self.user_data = pickle.load(bdd)
-        else:
+        if os.path.isfile(
+            bdd_login_filename
+        ):  # if bdd_login_filename points to a regular / existing file
+            with open(
+                bdd_login_filename, "rb"
+            ) as bdd:  # open it for reading in binary mode as bdd
+                self.user_data = pickle.load(
+                    bdd
+                )  # use pickle to load bdd binary file byte stream, convert it to a Python object, and store it in .user_data
+        else:  # else, use the following default values for .user_data
             self.user_data = {
                 "alexandre_winger": "user",
                 "fabrice_charraud": "user",
@@ -17,12 +23,20 @@ class BddLogin:
                 "omar_choa": "user",
                 "alban_thuet": "admin",
             }
-            with open(bdd_login_filename, "wb") as bdd:
-                pickle.dump(self.user_data, bdd)
+            with open(
+                bdd_login_filename, "wb"
+            ) as bdd:  # create bdd_login_filename as a new file and open it for writing in binary mode as bdd
+                pickle.dump(
+                    self.user_data, bdd
+                )  # use pickle to convert .user_data from a Python object to a binary file byte stream, store it in bdd, and write bdd to bdd_login_filename
 
-    def is_(self, rights, login, password):
-        key = f"{login}_{password}"
-        if key in self.user_data.keys() and rights == self.user_data[key]:
+    def is_(
+        self, rights, login, password
+    ):  # define custom method .is_() with attributes .rights, .login, .password
+        key = f"{login}_{password}"  # construct authentication chain
+        if (
+            key in self.user_data.keys() and rights == self.user_data[key]
+        ):  # if authentication chain is in .user_data dictionary keys, and if requested rights match corresponding .user_data dictionary value
             return True
         return False
 
