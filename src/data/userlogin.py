761168efdf4f40cebc1dcd72c6fbd1abe82faeee
Omar Choa <omar.choa@gmail.com>
2024-03-07 11:04:25 +0100

    Revert "Update userlogin.py"
    This reverts commit dbda941154615d616e3d642401cd4952fff45376.

--- src/data/userlogin.py
+++ src/data/userlogin.py
@@ -30,13 +30,9 @@ class BddLogin:
                     self.user_data, bdd
                 )  # use pickle to convert .user_data from a Python object to a binary file byte stream, store it in bdd, and write bdd to bdd_login_filename
 
-    def is_(
-        self, rights, login, password
-    ):  # define custom method .is_() with attributes .rights, .login, .password
-        key = f"{login}_{password}"  # construct authentication chain
-        if (
-            key in self.user_data.keys() and rights == self.user_data[key]
-        ):  # if authentication chain is in .user_data dictionary keys, and if requested rights match corresponding .user_data dictionary value
+    def is_(self, rights, login, password):
+        key = f"{login}_{password}"
+        if key in self.user_data and self.user_data[key] == rights:
             return True
         return False
 
