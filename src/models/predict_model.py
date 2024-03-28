1ece71ee0d4d2622fa6b3bc6d1d28a8cee94f4f0
Omar Choa <omar.choa@gmail.com>
2024-03-28 19:04:23 +0100

    refactor: replace paths using `config.paths`

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,17 +1,20 @@
-
 import joblib
 import pandas as pd
 import sys
 import json
+from config import paths
+
 
 # Load your saved model
-loaded_model = joblib.load("./src/models/trained_model.joblib")
+loaded_model = joblib.load(paths.MODEL_TRAINED)
+
 
 def predict_model(features):
     input_df = pd.DataFrame([features])
     print(input_df)
     prediction = loaded_model.predict(input_df)
     return prediction
+
 
 def get_feature_values_manually(feature_names):
     features = {}
@@ -20,13 +23,14 @@ def get_feature_values_manually(feature_names):
         features[feature_name] = feature_value
     return features
 
+
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         json_file = sys.argv[1]
-        with open(json_file, 'r') as file:
+        with open(json_file, "r") as file:
             features = json.load(file)
     else:
-        X_train = pd.read_csv("data/preprocessed/X_train.csv")
+        X_train = pd.read_csv(paths.X_TRAIN)
         feature_names = X_train.columns.tolist()
         features = get_feature_values_manually(feature_names)
 
