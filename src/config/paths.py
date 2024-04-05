29cd316017186250f5f44a133ec5c52e01b8cfca
Omar Choa <omar.choa@gmail.com>
2024-04-05 20:27:37 +0200

    refactor: delete cloud references

--- src/config/paths.py
+++ src/config/paths.py
@@ -2,9 +2,6 @@ from pathlib import Path
 import os
 
 
-# bucket
-BUCKET = "dec23-mlops-accidents"
-
 # root path
 if os.environ.get("CONTAINERIZED") == "yes":
     ROOT = "."
@@ -26,14 +23,9 @@ PREDS_UNLABELED = Path(LOGS, "preds_call.jsonl")
 PREDS_LABELED = Path(LOGS, "preds_labeled.jsonl")
 
 # model paths
-MODEL_SAVES_LOCAL = Path(ROOT, "models")
-MODEL_SAVES_CLOUD = "https://{bucket}.s3.eu-west-3.amazonaws.com/models".format(
-    bucket=BUCKET
-)
-MODEL_TRAINED_LOCAL = Path(MODEL_SAVES_LOCAL, "trained_model.joblib")
-MODEL_TRAINED_NEW_LOCAL = Path(MODEL_SAVES_LOCAL, "new_trained_model.joblib")
-MODEL_TRAINED_CLOUD = Path("models", "trained_model.joblib")
-MODEL_TRAINED_NEW_CLOUD = Path("models", "new_trained_model.joblib")
+MODEL_SAVES = Path(ROOT, "models")
+MODEL_TRAINED = Path(MODEL_SAVES, "trained_model.joblib")
+MODEL_TRAINED_NEW = Path(MODEL_SAVES, "new_trained_model.joblib")
 
 # other paths
 USERS = Path(ROOT, "src", "features", "api", "users_db_bis.json")
