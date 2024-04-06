04363b59b2e669b1c8ccda0e7844f82c92f78330
Omar Choa <omar.choa@gmail.com>
2024-04-06 15:31:44 +0200

    feat: add `docker-setup-training.sh`
    scripts to setup docker environment for standalone execution of training microservice

---
+++ docker-setup-training-linux.sh
@@ -0,0 +1,29 @@
+# script to setup docker environment on linux for standalone execution of training microservice
+
+
+# build dummy microservice image
+docker image build -f ./src/docker/dummy/dummy.Dockerfile -t omarchoa/shield:dummy .
+
+# push dummy microservice image
+docker image push omarchoa/shield:dummy
+
+# build training microservice image
+docker image build -f ./src/docker/training/training.Dockerfile -t omarchoa/shield:training .
+
+# push training microservice image
+docker image push omarchoa/shield:training
+
+# create shield network
+docker network create shield
+
+# create data volume
+docker volume create data
+
+# populate data volume
+docker cp ./data /var/lib/docker/volumes/data/_data/
+
+# create models volume
+docker volume create models
+
+# launch docker-compose
+docker-compose -f docker-compose-training.yml up
