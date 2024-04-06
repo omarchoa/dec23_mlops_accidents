9a393e88f5fc1ae8476a7b26153a289827144770
Omar Choa <omar.choa@gmail.com>
2024-04-06 12:55:03 +0200

    Revert "share `config` package with `feature/docker/4-training` branch"

--- src/config/test_import_paths.py
+++
@@ -1,8 +0,0 @@
-# before executing this script, execute the following command, replacing /path/to/your/project/ with the absolute path to the src folder:
-# - mac/linux: export PYTHONPATH="${PYTHONPATH}:/path/to/your/project/"
-# - windows: set PYTHONPATH=%PYTHONPATH%;C:\path\to\your\project\
-
-from config import paths
-
-
-print("Your root path is:", paths.ROOT)
