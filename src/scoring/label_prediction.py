135fc669057a0b856ca75946b7788a7b3ee1ee95
Omar Choa <omar.choa@gmail.com>
2024-04-07 22:34:41 +0200

    feat: add `label_prediction.py`
    for endpoint `/label_prediction`

---
+++ src/scoring/label_prediction.py
@@ -0,0 +1,37 @@
+# imports
+import sys
+import json
+from config import paths
+
+
+# load prediction label from input file
+input = sys.argv[1]
+with open(input, "r") as file:
+    pred_label = json.load(file)
+
+# load unlabeled predictions from logs
+with open(paths.LOGS_PREDS_UNLABELED, "r") as file:
+    preds_unlabeled = [json.loads(line) for line in file]
+
+# label corresponding prediction
+record_exists = "no"
+record_to_update = {}
+for record in preds_unlabeled:
+    ## pull up record corresponding to input request_id
+    if int(record["request_id"]) == pred_label["request_id"]:
+        record_exists = "yes"
+        record_to_update = record
+
+        ## update verified_prediction field with input y_true
+        record_to_update["verified_prediction"] = pred_label["y_true"]
+
+        ## save updated record to labeled prediction logs
+        record_updated_json = json.dumps(obj=record_to_update)
+        with open(paths.LOGS_PREDS_LABELED, "a") as file:
+            file.write(record_updated_json + "\n")
+
+# print response
+if record_exists == "yes":
+    print("Record updated. Thank you for your feedback.")
+else:
+    print("Record not found. Please provide a valid request_id.")
