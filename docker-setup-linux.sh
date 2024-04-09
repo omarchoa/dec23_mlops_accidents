fd98109d483e6a8d9a7956d7106f8b2c2d61d707
Omar Choa <omar.choa@gmail.com>
2024-04-09 11:32:58 +0200

    feat: include image building in setup files

--- docker-setup-linux.sh
+++ docker-setup-linux.sh
@@ -1,6 +1,12 @@
 # script to setup docker environment on linux for standalone execution of training, prediction, and scoring microservices
 
 
+# build images
+docker image build -f ./src/docker/dummy/dummy.Dockerfile -t omarchoa/shield:dummy .
+docker image build -f ./src/docker/training/training.Dockerfile -t omarchoa/shield:training .
+docker image build -f ./src/docker/prediction/prediction.Dockerfile -t omarchoa/shield:prediction .
+docker image build -f ./src/docker/scoring/scoring.Dockerfile -t omarchoa/shield:scoring .
+
 # create network
 docker network create shield
 
