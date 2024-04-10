c44a4cc66566f07d887c90263031784379d23857
Omar Choa <omar.choa@gmail.com>
2024-04-10 16:58:47 +0200

    refactor: move setup files to `src/scripts`

--- src/scripts/setup_dev_linux.sh
+++ src/scripts/setup_dev_linux.sh
@@ -1,4 +1,4 @@
-# script to setup docker environment on linux for standalone execution of training, prediction, and scoring microservices
+# script to setup docker environment on linux
 
 
 # create network
@@ -11,14 +11,14 @@ docker volume create --name models
 docker volume create --name users
 
 # populate volumes
-sudo docker cp ./data \
+sudo docker cp data \
     /var/lib/docker/volumes/data/_data/
-sudo docker cp ./logs \
+sudo docker cp logs \
     /var/lib/docker/volumes/logs/_data/
-sudo docker cp ./models \
+sudo docker cp models \
     /var/lib/docker/volumes/models/_data/
-sudo docker cp ./src/docker/bdd/users_db_bis.json \
+sudo docker cp src/docker/bdd/users_db_bis.json \
     /var/lib/docker/volumes/users/_data/
 
 # launch docker-compose
-docker-compose -f docker-compose-dev.yml up
+docker-compose -f src/docker/docker-compose-dev.yml up
--- src/scripts/setup_dev_mac.sh
+++ src/scripts/setup_dev_mac.sh
@@ -1,4 +1,4 @@
-# script to setup docker environment on mac for standalone execution of training, prediction, and scoring microservices
+# script to setup docker environment on mac
 
 
 # create network
@@ -11,19 +11,19 @@ docker volume create --name models
 docker volume create --name users
 
 # populate volumes
-docker image build -f ./src/docker/dummy/dummy.Dockerfile -t omarchoa/shield:dummy .
+docker image build -f src/docker/dummy/dummy.Dockerfile -t omarchoa/shield:dummy .
 docker container run -d --rm --name dummy \
     -v data:/home/shield/data \
     -v logs:/home/shield/logs \
     -v models:/home/shield/models \
     -v users:/home/shield/users \
     omarchoa/shield:dummy
-sudo docker cp ./data dummy:/home/shield
-sudo docker cp ./logs dummy:/home/shield
-sudo docker cp ./models dummy:/home/shield
-sudo docker cp ./src/docker/bdd/users_db_bis.json \
+sudo docker cp data dummy:/home/shield
+sudo docker cp logs dummy:/home/shield
+sudo docker cp models dummy:/home/shield
+sudo docker cp src/docker/bdd/users_db_bis.json \
     dummy:/home/shield/users
 docker container stop dummy
 
 # launch docker-compose
-docker-compose -f docker-compose-dev.yml up
+docker-compose -f src/docker/docker-compose-dev.yml up
