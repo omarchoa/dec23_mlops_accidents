97e70fd3f695622e3e9efd73ed75f3280d68e8a8
Omar Choa <omar.choa@gmail.com>
2024-04-11 22:20:42 +0200

    refactor: delete shell scripts
    no longer necessary as all setup steps are now directly handled by docker compose

--- src/docker/all_in_one.sh
+++
@@ -1,8 +0,0 @@
-docker system prune -af
-# docker container prune -f
-docker image build -f ./src/docker/bdd/Dockerfile ./src/docker/bdd -t dummyfcd/bdd_container:0.0.1
-# docker image rm dummyfcd/data_container:0.0.1
-docker image build -f ./src/docker/data/Dockerfile ./src/docker/data -t dummyfcd/data_container:0.0.1
-# docker image rm dummyfcd/gateway_container:0.0.1
-docker image build -f ./src/docker/gateway/Dockerfile ./src/docker/gateway -t dummyfcd/gateway_container:0.0.1
-docker-compose -f src/docker/docker-compose.yml up
