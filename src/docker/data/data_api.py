163fc3ebebdb02f36ba90086c06b5340e2e99d43
Omar Choa <omar.choa@gmail.com>
2024-04-11 21:38:15 +0200

    refactor: harmonize code & docs w `gateway_api.py`

--- src/docker/data/data_api.py
+++ src/docker/data/data_api.py
@@ -1,37 +1,34 @@
+# imports
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-
-# from pathlib import Path
-# import sys
-
-# local library
-# root_path = Path(os.path.realpath(__file__)).parents[3]
-# sys.path.append(os.path.join(root_path, "src", "data"))
-import containerdata  # will be at the same level in the container
-
-# ---------------------------- API --------------------------------------------
-
-api = FastAPI(
-    title="SHIELD",
-    description="API for downloading raw data files",
-    version="0.1",
-)
+import containerdata  ## custom library
 
 
-@api.get('/status', name="check API", tags=['GET'])
-async def is_fonctional():
-    """
-    check if the API is running
-    """
-    return {"DATA API is running"}
-
-
+# define input data model for endpoint /update
 class YearRange(BaseModel):
     start_year: int
     end_year: int
 
 
-@api.post('/update_data', tags=['UPDATE'])
-async def update_data(year_range: YearRange):
-    containerdata.Data(year_range.start_year, year_range.end_year, '/mounted_data_container_side')
-    return {"Data updated"}
+# create fastapi instance
+api = FastAPI(title="SHIELD Microservice API - Data")
+
+
+# endpoint - status
+@api.get(path="/status", tags=["STATUS"], name="check data microservice API status")
+async def status():
+    result = "The data microservice API is up."
+    return JSONResponse(content=result)
+
+
+# endpoint - update
+@api.post(path="/update", tags=["PROCESS"], name="update data")
+async def update(year_range: YearRange):
+    containerdata.Data(
+        start_year=year_range.start_year,
+        end_year=year_range.end_year,
+        data_path="home/shield/data",
+    )
+    result = "Data updated."
+    return JSONResponse(content=result)
