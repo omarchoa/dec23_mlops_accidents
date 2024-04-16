f24a85af1f043eb27001bd1b88bcb3f82412b4f1
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-16 09:02:50 +0200

    Update testing.py
    Ajout des endpoints pour la fonction test_data_status

--- src/docker/testing/testing.py
+++ src/docker/testing/testing.py
@@ -1,8 +1,24 @@
 import requests
 
-
-def test_gateway_status():
-    response = requests.get("http://gateway:8001/gateway/status")
+def test_data_status(endpoint):
+    url = f"http://gateway:8001/{endpoint}"
+    response = requests.get(url)
     assert response.status_code == 200
-    message = "Test /gateway/status: PASSED"
-    print(message)
+    print(f"Test {url}: PASSED")
+
+endpoints = [
+    "gateway/status",
+    "data/status",
+    "data/update",
+    "training/status",
+    "training/train",
+    "prediction/status",
+    "prediction/test",
+    "prediction/call",
+    "scoring/status",
+    "scoring/label_prediction",
+    "scoring/update_f1_score"
+]
+
+for endpoint in endpoints:
+    test_data_status(endpoint)
