859a5b50bf0b5131a21a19bd42de832259267943
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-16 09:45:08 +0200

    Update testing.py
    Correction de l'erreur de configuration du test due à la présence d'un paramètre inattendu "endpoint" dans la fonction de test, en le supprimant et en réorganisant la logique pour itérer sur une liste prédéfinie d'endpoints.

--- src/docker/testing/testing.py
+++ src/docker/testing/testing.py
@@ -1,27 +1,27 @@
 import requests
 
-def test_data_status(endpoint):
-    url = f"http://gateway:8001/{endpoint}"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception if the status code is not 200
-        print(f"Test {url}: PASSED")
-    except requests.RequestException as e:
-        print(f"Test {url}: FAILED - {e}")
+def test_data_status():
+    endpoints = [
+        "gateway/status",
+        "data/status",
+        "data/update",
+        "training/status",
+        "training/train",
+        "prediction/status",
+        "prediction/test",
+        "prediction/call",
+        "scoring/status",
+        "scoring/label_prediction",
+        "scoring/update_f1_score"
+    ]
 
-endpoints = [
-    "gateway/status",
-    "data/status",
-    "data/update",
-    "training/status",
-    "training/train",
-    "prediction/status",
-    "prediction/test",
-    "prediction/call",
-    "scoring/status",
-    "scoring/label_prediction",
-    "scoring/update_f1_score"
-]
+    for endpoint in endpoints:
+        url = f"http://gateway:8001/{endpoint}"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an exception if the status code is not 200
+            print(f"Test {url}: PASSED")
+        except requests.RequestException as e:
+            print(f"Test {url}: FAILED - {e}")
 
-for endpoint in endpoints:
-    test_data_status(endpoint)
+test_data_status()
