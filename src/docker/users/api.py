e99cbf0b114953c61e4f8f3ee2d071d9f7560dec
Omar Choa <omar.choa@gmail.com>
2024-04-18 14:20:37 +0200

    refactor: apply automatic pep8 formatting w/ black

--- src/docker/users/api.py
+++ src/docker/users/api.py
@@ -20,15 +20,21 @@ class OldUser(BaseModel):
 
 # define function to get all users from database
 def get_users_db():
-    mariadb_engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=False)  # echo is for debug mode
+    mariadb_engine = create_engine(
+        SQLALCHEMY_DATABASE_URI, echo=False
+    )  # echo is for debug mode
     with mariadb_engine.connect() as connection:
-        results = connection.execute(text('SELECT * FROM users_table;'))
-        users_db = {login: {"pwd": pwd, "admin":admin} for login, pwd, admin in results}
+        results = connection.execute(text("SELECT * FROM users_table;"))
+        users_db = {
+            login: {"pwd": pwd, "admin": admin} for login, pwd, admin in results
+        }
     return users_db
 
 
 # define database uri
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@mariadb_container:3306/shield_project_db'
+SQLALCHEMY_DATABASE_URI = (
+    "mysql+pymysql://user:password@mariadb_container:3306/shield_project_db"
+)
 
 
 # create fastapi instance
@@ -36,11 +42,7 @@ api = FastAPI(title="SHIELD Microservice API - Users")
 
 
 # endpoint - status
-@api.get(
-    path="/status",
-    tags=["STATUS"],
-    name="check users microservice API status"
-)
+@api.get(path="/status", tags=["STATUS"], name="check users microservice API status")
 async def status():
     result = "The users microservice API is up."
     return JSONResponse(content=result)
@@ -57,34 +59,36 @@ async def all():
 
 
 # endpoint - register
-@api.post(
-    path="/register",
-    tags=["PROCESSES"],
-    name="register user"
-)
+@api.post(path="/register", tags=["PROCESSES"], name="register user")
 async def register(new_user: NewUser):
     """Add a user to the database"""
 
-    mariadb_engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=False)  # echo is for debug mode
+    mariadb_engine = create_engine(
+        SQLALCHEMY_DATABASE_URI, echo=False
+    )  # echo is for debug mode
     with mariadb_engine.connect() as connection:
-        connection.execute(text(f'INSERT INTO users_table VALUES ("{new_user.username}", "{new_user.password}", "{new_user.rights}");'))
-        connection.execute(text('COMMIT;'))
+        connection.execute(
+            text(
+                f'INSERT INTO users_table VALUES ("{new_user.username}", "{new_user.password}", "{new_user.rights}");'
+            )
+        )
+        connection.execute(text("COMMIT;"))
     result = "User added."
     return JSONResponse(content=result)
 
 
 # endpoint - remove
-@api.delete(
-    path="/remove",
-    tags=["PROCESSES"],
-    name="remove user"
-)
+@api.delete(path="/remove", tags=["PROCESSES"], name="remove user")
 async def remove(old_user: OldUser):
     """Remove a user from the database"""
 
-    mariadb_engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=False)  # echo is for debug mode
+    mariadb_engine = create_engine(
+        SQLALCHEMY_DATABASE_URI, echo=False
+    )  # echo is for debug mode
     with mariadb_engine.connect() as connection:
-        connection.execute(text(f'DELETE FROM users_table WHERE login = "{old_user.user}";'))
-        connection.execute(text('COMMIT;'))
+        connection.execute(
+            text(f'DELETE FROM users_table WHERE login = "{old_user.user}";')
+        )
+        connection.execute(text("COMMIT;"))
     result = "User removed."
     return JSONResponse(content=result)
