1bb1a81c5fa9aa5ea63a01e6bdef28be0c80cfac
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-22 18:05:00 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -18,5 +18,10 @@ def main():
 def show_homepage():
     st.write('Welcome to SHIELD. This application allows you to predict road accident priority levels.')
 
+def show_features():
+    st.markdown("<h1 id='features' style='text-align: center;'>Accident Features</h1>", unsafe_allow_html=True)
+    feature1 = st.text_input('Feature 1')
+    feature2 = st.text_input('Feature 2')
+
 if __name__ == '__main__':
     main()
