c6044c86cb3bbeaf6dcb8ff3cea7fb0f26517a51
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-22 18:02:00 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,21 +5,17 @@ def main():
 
     # Menu in the sidebar
     st.sidebar.title('Menu')
-    st.sidebar.markdown("", unsafe_allow_html=True)
+    st.sidebar.markdown("[Home](#home)", unsafe_allow_html=True)
+    st.sidebar.markdown("[Features](#features)", unsafe_allow_html=True)
 
-    # Display the corresponding page based on the selection
-    if st.sidebar.button("Home"):
+    # Afficher la page correspondante en fonction de la sélection
+    selected_page = st.sidebar.button("Home")
+
+    if selected_page:
         show_homepage()
-    elif st.sidebar.button("Features"):
-        show_features()
 
 def show_homepage():
     st.write('Welcome to SHIELD. This application allows you to predict road accident priority levels.')
-
-def show_features():
-    st.markdown("<h1 id='features' style='text-align: center;'>Accident Features</h1>", unsafe_allow_html=True)
-    feature1 = st.text_input('Feature 1')
-    feature2 = st.text_input('Feature 2')
 
 if __name__ == '__main__':
     main()
