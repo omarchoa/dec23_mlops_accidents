bc608ab3d3d664253d64c3de0109ac9b7fd46164
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-22 17:48:19 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,6 +3,9 @@ import streamlit as st
 def main():
     st.title('SHIELD: Prédiction des niveaux de priorité des accidents de la route')
 
+    st.sidebar.title('Menu')
+    page1 = st.sidebar.text_input('Page 1')
+
     st.markdown('<style>div.Widget.row-widget.stRadio > div{flex-direction: row;}</style>', unsafe_allow_html=True)
 
     st.markdown("<h1 style='text-align: center;'>Caractéristiques de l'accident</h1>", unsafe_allow_html=True)
