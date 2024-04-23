d382e51cfd94e01b71bb30c4d704fe32c5d4b864
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-23 09:33:53 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -14,7 +14,7 @@ def main():
         show_features()
 
 def show_homepage():
-    col1, col2, col3 = st.columns([1, 3, 1])
+    col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         st.image("/mount/src/dec23_mlops_accidents/streamlit/images/bouclier.png", caption='SHIELD Logo', width=150)
         st.markdown("<h1 style='text-align:center;'>Welcome to SHIELD</h1>", unsafe_allow_html=True)
