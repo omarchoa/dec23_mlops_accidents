1d50caa1e89a6b5f448032638c75672a906188c0
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-23 10:49:59 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -74,8 +74,7 @@ def show_graph():
     st.markdown("<h1 id='graph' style='text-align: center;'>Graph</h1>", unsafe_allow_html=True)
 
     # Generate random data for the chart
-    data = np.random.randn(20)
-
+    data = [random.randint(0, 10) for _ in range(20)]
     # Display the line chart
     st.line_chart(data)
     
