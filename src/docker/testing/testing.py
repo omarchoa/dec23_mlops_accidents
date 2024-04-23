9826d547701795dcf7fc6b35c3d941d7703bbba1
Omar Choa <omar.choa@gmail.com>
2024-04-23 21:56:33 +0200

    test: add `test_prediction_call()`

--- src/docker/testing/testing.py
+++ src/docker/testing/testing.py
@@ -6,6 +6,36 @@ header_admin = {"identification": "admin:4dmin"}
 payload_new_user = {"username": "antoine", "password": "jussieu", "rights": 0}
 payload_old_user = {"user": "antoine"}
 payload_year_range = {"start_year": 2021, "end_year": 2021}
+payload_input_data = {
+    "place": 10,
+    "catu": 3,
+    "sexe": 1,
+    "secu1": 0.0,
+    "year_acc": 2021,
+    "victim_age": 60,
+    "catv": 2,
+    "obsm": 1,
+    "motor": 1,
+    "catr": 3,
+    "circ": 2,
+    "surf": 1,
+    "situ": 1,
+    "vma": 50,
+    "jour": 7,
+    "mois": 12,
+    "lum": 5,
+    "dep": 77,
+    "com": 77317,
+    "agg_": 2,
+    "inter": 1,
+    "atm": 0,
+    "col": 6,
+    "lat": 48.6,
+    "long": 2.89,
+    "hour": 17,
+    "nb_victim": 2,
+    "nb_vehicules": 1,
+}
 
 
 def test_gateway_status():
@@ -102,4 +132,16 @@ def test_prediction_test():
     )
     assert response.status_code == 200
     message = "Test /prediction/test: PASSED"
+    print(message)
+
+
+def test_prediction_call():
+    # time.sleep(5)
+    response = requests.post(
+        url="http://gateway:8001/prediction/call",
+        json=payload_input_data,
+        headers=header_admin,
+    )
+    assert response.status_code == 200
+    message = "Test /prediction/call: PASSED"
     print(message)
