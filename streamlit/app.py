9c5d4ae9a4bd345bebac34737e9ff6f590a24e4a
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-24 16:45:16 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -51,20 +51,20 @@ def show_features():
         "sexe_usager": st.select_slider("Sexe de l'usager", options={1: "Masculin", 2: "Féminin"}),
         "equipement_securite": st.select_slider("Équipement de sécurité", options={-1: "Non renseigné", 0: "Aucun équipement", 1: "Ceinture", 2: "Casque", 3: "Dispositif enfants", 4: "Gilet réfléchissant", 5: "Airbag (2RM/3RM)", 6: "Gants (2RM/3RM)", 7: "Gants + Airbag (2RM/3RM)", 8: "Non déterminable", 9: "Autre"}),
         "age_victime": st.slider("Âge de la ou des victimes", min_value=0, max_value=100, step=1),
-        "categorie_vehicule": map_feature(st.select_slider("Catégorie du véhicule", options={i: i for i in range(100)}), "categorie_vehicule"),
-        "obstacle_mobile": map_feature(st.select_slider("Obstacle mobile heurté", options={-1: "Non renseigné", 0: "Aucun", 1: "Piéton", 2: "Véhicule", 4: "Véhicule sur rail", 5: "Animal domestique", 6: "Animal sauvage", 9: "Autre"}), "obstacle_mobile"),
-        "type_motorisation": map_feature(st.select_slider("Type de motorisation du véhicule", options={-1: "Non renseigné", 0: "Inconnue", 1: "Hydrocarbures", 2: "Hybride électrique", 3: "Electrique", 4: "Hydrogène", 5: "Humaine", 6: "Autre"}), "type_motorisation"),
-        "categorie_route": map_feature(st.select_slider("Catégorie de route", options={1: "Autoroute", 2: "Route nationale", 3: "Route Départementale", 4: "Voie Communales", 5: "Hors réseau public", 6: "Parc de stationnement ouvert à la circulation publique", 7: "Routes de métropole urbaine", 9: "Autre"}), "categorie_route"),
-        "regime_circulation": map_feature(st.select_slider("Régime de circulation", options={-1: "Non renseigné", 1: "A sens unique", 2: "Bidirectionnelle", 3: "A chaussées séparées", 4: "Avec voies d’affectation variable"}), "regime_circulation"),
-        "etat_surface": map_feature(st.select_slider("État de la surface", options={-1: "Non renseigné", 1: "Normale", 2: "Mouillée", 3: "Flaques", 4: "Inondée", 5: "Enneigée", 6: "Boue", 7: "Verglacée", 8: "Corps gras – huile", 9: "Autre"}), "etat_surface"),
-        "situation_accident": map_feature(st.select_slider("Situation de l’accident", options={-1: "Non renseigné", 0: "Aucun", 1: "Sur chaussée", 2: "Sur bande d’arrêt d’urgence", 3: "Sur accotement", 4: "Sur trottoir", 5: "Sur piste cyclable", 6: "Sur autre voie spéciale", 8: "Autres"}), "situation_accident"),
+        "categorie_vehicule": st.select_slider("Catégorie du véhicule", options={i: i for i in range(100)}), "categorie_vehicule"),
+        "obstacle_mobile": st.select_slider("Obstacle mobile heurté", options={-1: "Non renseigné", 0: "Aucun", 1: "Piéton", 2: "Véhicule", 4: "Véhicule sur rail", 5: "Animal domestique", 6: "Animal sauvage", 9: "Autre"}), "obstacle_mobile"),
+        "type_motorisation": st.select_slider("Type de motorisation du véhicule", options={-1: "Non renseigné", 0: "Inconnue", 1: "Hydrocarbures", 2: "Hybride électrique", 3: "Electrique", 4: "Hydrogène", 5: "Humaine", 6: "Autre"}), "type_motorisation"),
+        "categorie_route": st.select_slider("Catégorie de route", options={1: "Autoroute", 2: "Route nationale", 3: "Route Départementale", 4: "Voie Communales", 5: "Hors réseau public", 6: "Parc de stationnement ouvert à la circulation publique", 7: "Routes de métropole urbaine", 9: "Autre"}), "categorie_route"),
+        "regime_circulation": st.select_slider("Régime de circulation", options={-1: "Non renseigné", 1: "A sens unique", 2: "Bidirectionnelle", 3: "A chaussées séparées", 4: "Avec voies d’affectation variable"}), "regime_circulation"),
+        "etat_surface": st.select_slider("État de la surface", options={-1: "Non renseigné", 1: "Normale", 2: "Mouillée", 3: "Flaques", 4: "Inondée", 5: "Enneigée", 6: "Boue", 7: "Verglacée", 8: "Corps gras – huile", 9: "Autre"}), "etat_surface"),
+        "situation_accident": st.select_slider("Situation de l’accident", options={-1: "Non renseigné", 0: "Aucun", 1: "Sur chaussée", 2: "Sur bande d’arrêt d’urgence", 3: "Sur accotement", 4: "Sur trottoir", 5: "Sur piste cyclable", 6: "Sur autre voie spéciale", 8: "Autres"}), "situation_accident"),
         "vitesse_max_autorisee": st.slider("Vitesse maximale autorisée", min_value=0, max_value=300, step=1),
-        "lumiere": map_feature(st.select_slider("Lumière", options={1: "Plein jour", 2: "Crépuscule ou aube", 3: "Nuit sans éclairage public", 4: "Nuit avec éclairage public non allumé", 5: "Nuit avec éclairage public allumé"}), "lumiere"),
+        "lumiere": st.select_slider("Lumière", options={1: "Plein jour", 2: "Crépuscule ou aube", 3: "Nuit sans éclairage public", 4: "Nuit avec éclairage public non allumé", 5: "Nuit avec éclairage public allumé"}), "lumiere"),
         "commune": st.text_input("Commune (Code INSEE)"),
-        "localisation": map_feature(st.select_slider("Localisation", options={1: "Hors agglomération", 2: "En agglomération"}), "localisation"),
-        "intersection": map_feature(st.select_slider("Intersection", options={1: "Hors intersection", 2: "Intersection en X", 3: "Intersection en T", 4: "Intersection en Y", 5: "Intersection à plus de 4 branches", 6: "Giratoire", 7: "Place", 8: "Passage à niveau", 9: "Autre intersection"}), "intersection"),
-        "conditions_atmospheriques": map_feature(st.select_slider("Conditions atmosphériques", options={-1: "Non renseigné", 1: "Normale", 2: "Pluie légère", 3: "Pluie forte", 4: "Neige - grêle", 5: "Brouillard - fumée", 6: "Vent fort - tempête", 7: "Temps éblouissant", 8: "Temps couvert", 9: "Autre"}), "conditions_atmospheriques"),
-        "type_collision": map_feature(st.select_slider("Type de collision", options={-1: "Non renseigné", 1: "Deux véhicules - frontale", 2: "Deux véhicules – par l’arrière", 3: "Deux véhicules – par le coté", 4: "Trois véhicules et plus – en chaîne", 5: "Trois véhicules et plus - collisions multiples", 6: "Autre collision", 7: "Sans collision"}), "type_collision"),
+        "localisation": st.select_slider("Localisation", options={1: "Hors agglomération", 2: "En agglomération"}), "localisation"),
+        "intersection": st.select_slider("Intersection", options={1: "Hors intersection", 2: "Intersection en X", 3: "Intersection en T", 4: "Intersection en Y", 5: "Intersection à plus de 4 branches", 6: "Giratoire", 7: "Place", 8: "Passage à niveau", 9: "Autre intersection"}), "intersection"),
+        "conditions_atmospheriques": st.select_slider("Conditions atmosphériques", options={-1: "Non renseigné", 1: "Normale", 2: "Pluie légère", 3: "Pluie forte", 4: "Neige - grêle", 5: "Brouillard - fumée", 6: "Vent fort - tempête", 7: "Temps éblouissant", 8: "Temps couvert", 9: "Autre"}), "conditions_atmospheriques"),
+        "type_collision": st.select_slider("Type de collision", options={-1: "Non renseigné", 1: "Deux véhicules - frontale", 2: "Deux véhicules – par l’arrière", 3: "Deux véhicules – par le coté", 4: "Trois véhicules et plus – en chaîne", 5: "Trois véhicules et plus - collisions multiples", 6: "Autre collision", 7: "Sans collision"}), "type_collision"),
         "latitude": st.slider("Latitude", min_value=-90.0, max_value=90.0, step=0.001),
         "longitude": st.slider("Longitude", min_value=-180.0, max_value=180.0, step=0.001),
     }
@@ -74,24 +74,6 @@ def show_features():
         # Effectuer le traitement ici
         pass
 
-def map_feature(index, feature_type):
-    mappings = {
-        "categorie_vehicule": {
-            "00": "Indéterminable",
-            "01": "Bicyclette",
-            "02": "Cyclomoteur <50cm3",
-            # Ajouter d'autres éléments ici
-        },
-        "obstacle_mobile": {
-            -1: "Non renseigné",
-            0: "Aucun",
-            1: "Piéton",
-            # Ajouter d'autres éléments ici
-        },
-        # Ajouter d'autres dictionnaires de mapping ici
-    }
-
-    return mappings[feature_type][index]
 
 def show_graph():
     st.markdown("<h1 id='graph' style='text-align: center;'>Graphique de Prédiction</h1>", unsafe_allow_html=True)
