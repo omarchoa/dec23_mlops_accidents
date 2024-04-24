373b996ee3e0d1e0b9c679d49423c4d856844228
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-24 17:04:45 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -68,11 +68,11 @@ def show_features():
     longitude = st.slider("Longitude", min_value=-180.0, max_value=180.0, step=0.001)
 
     st.write("")  # Ajouter de l'espace vertical pour créer une nouvelle ligne
-if st.button("Valider"):
-    # Effectuer le traitement ici
-    gravity = determine_gravity(jour_accident, heure_accident, nombre_victimes, age_victime)
-    gravity_text = translate_gravity(gravity)
-    st.write(f"Gravité de l'accident : {gravity_text}")
+    if st.button("Valider"):
+        # Effectuer le traitement ici
+        gravity = determine_gravity(jour_accident, heure_accident, nombre_victimes, age_victime)
+        gravity_text = translate_gravity(gravity)
+        st.write(f"Gravité de l'accident : {gravity_text}")
     
 def determine_gravity(jour_accident, heure_accident, nombre_victimes, age_victime):
     # Exemple de logique de détermination de la gravité fictive
