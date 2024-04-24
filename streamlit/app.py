4c872bbee319b047ee441e55aa0198f0a4c5bc70
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-24 16:49:56 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -40,40 +40,37 @@ def show_features():
     st.markdown("<h1 id='features' style='text-align: center;'>Caractéristiques de l'Accident</h1>", unsafe_allow_html=True)
    
     # Caractéristiques
-features = {
-        "jour_accident": st.date_input("Jour de l'accident", value=None, min_value=None, max_value=None, key=None),
-        "heure_accident": st.slider("Heure de l'accident", min_value=0, max_value=23, step=1),
-        "departement": st.text_input("Département (Code INSEE)"),
-        "place_occupée": st.slider("Place occupée dans le véhicule", min_value=0, max_value=10, step=1, help="10 – Piéton (non applicable)"),
-        "nombre_victimes": st.slider("Nombre de victimes", min_value=0, max_value=100, step=1),
-        "nombre_vehicules": st.text_input("Nombre de véhicules impliqués"),
-        "categorie_usager": st.select_slider("Catégorie d'usager", options={1: "Conducteur", 2: "Passager", 3: "Piéton"}),
-        "sexe_usager": st.select_slider("Sexe de l'usager", options={1: "Masculin", 2: "Féminin"}),
-        "equipement_securite": st.select_slider("Équipement de sécurité", options={-1: "Non renseigné", 0: "Aucun équipement", 1: "Ceinture", 2: "Casque", 3: "Dispositif enfants", 4: "Gilet réfléchissant", 5: "Airbag (2RM/3RM)", 6: "Gants (2RM/3RM)", 7: "Gants + Airbag (2RM/3RM)", 8: "Non déterminable", 9: "Autre"}),
-        "age_victime": st.slider("Âge de la ou des victimes", min_value=0, max_value=100, step=1),
-        "categorie_vehicule": st.select_slider("Catégorie du véhicule", options={i: i for i in range(100)}), "categorie_vehicule"),
-        "obstacle_mobile": st.select_slider("Obstacle mobile heurté", options={-1: "Non renseigné", 0: "Aucun", 1: "Piéton", 2: "Véhicule", 4: "Véhicule sur rail", 5: "Animal domestique", 6: "Animal sauvage", 9: "Autre"}), "obstacle_mobile"),
-        "type_motorisation": st.select_slider("Type de motorisation du véhicule", options={-1: "Non renseigné", 0: "Inconnue", 1: "Hydrocarbures", 2: "Hybride électrique", 3: "Electrique", 4: "Hydrogène", 5: "Humaine", 6: "Autre"}), "type_motorisation"),
-        "categorie_route": st.select_slider("Catégorie de route", options={1: "Autoroute", 2: "Route nationale", 3: "Route Départementale", 4: "Voie Communales", 5: "Hors réseau public", 6: "Parc de stationnement ouvert à la circulation publique", 7: "Routes de métropole urbaine", 9: "Autre"}), "categorie_route"),
-        "regime_circulation": st.select_slider("Régime de circulation", options={-1: "Non renseigné", 1: "A sens unique", 2: "Bidirectionnelle", 3: "A chaussées séparées", 4: "Avec voies d’affectation variable"}), "regime_circulation"),
-        "etat_surface": st.select_slider("État de la surface", options={-1: "Non renseigné", 1: "Normale", 2: "Mouillée", 3: "Flaques", 4: "Inondée", 5: "Enneigée", 6: "Boue", 7: "Verglacée", 8: "Corps gras – huile", 9: "Autre"}), "etat_surface"),
-        "situation_accident": st.select_slider("Situation de l’accident", options={-1: "Non renseigné", 0: "Aucun", 1: "Sur chaussée", 2: "Sur bande d’arrêt d’urgence", 3: "Sur accotement", 4: "Sur trottoir", 5: "Sur piste cyclable", 6: "Sur autre voie spéciale", 8: "Autres"}), "situation_accident"),
-        "vitesse_max_autorisee": st.slider("Vitesse maximale autorisée", min_value=0, max_value=300, step=1),
-        "lumiere": st.select_slider("Lumière", options={1: "Plein jour", 2: "Crépuscule ou aube", 3: "Nuit sans éclairage public", 4: "Nuit avec éclairage public non allumé", 5: "Nuit avec éclairage public allumé"}), "lumiere"),
-        "commune": st.text_input("Commune (Code INSEE)"),
-        "localisation": st.select_slider("Localisation", options={1: "Hors agglomération", 2: "En agglomération"}), "localisation"),
-        "intersection": st.select_slider("Intersection", options={1: "Hors intersection", 2: "Intersection en X", 3: "Intersection en T", 4: "Intersection en Y", 5: "Intersection à plus de 4 branches", 6: "Giratoire", 7: "Place", 8: "Passage à niveau", 9: "Autre intersection"}), "intersection"),
-        "conditions_atmospheriques": st.select_slider("Conditions atmosphériques", options={-1: "Non renseigné", 1: "Normale", 2: "Pluie légère", 3: "Pluie forte", 4: "Neige - grêle", 5: "Brouillard - fumée", 6: "Vent fort - tempête", 7: "Temps éblouissant", 8: "Temps couvert", 9: "Autre"}), "conditions_atmospheriques"),
-        "type_collision": st.select_slider("Type de collision", options={-1: "Non renseigné", 1: "Deux véhicules - frontale", 2: "Deux véhicules – par l’arrière", 3: "Deux véhicules – par le coté", 4: "Trois véhicules et plus – en chaîne", 5: "Trois véhicules et plus - collisions multiples", 6: "Autre collision", 7: "Sans collision"}), "type_collision"),
-        "latitude": st.slider("Latitude", min_value=-90.0, max_value=90.0, step=0.001),
-        "longitude": st.slider("Longitude", min_value=-180.0, max_value=180.0, step=0.001),
-    }
+    jour_accident = st.date_input("Jour de l'accident", value=None, min_value=None, max_value=None, key=None)
+    heure_accident = st.slider("Heure de l'accident", min_value=0, max_value=23, step=1)
+    departement = st.text_input("Département (Code INSEE)")
+    place_occupée = st.slider("Place occupée dans le véhicule", min_value=0, max_value=10, step=1, help="10 – Piéton (non applicable)")
+    nombre_victimes = st.slider("Nombre de victimes", min_value=0, max_value=100, step=1)
+    nombre_vehicules = st.text_input("Nombre de véhicules impliqués")
+    categorie_usager = st.selectbox("Catégorie d'usager", options=["Conducteur", "Passager", "Piéton"])
+    sexe_usager = st.selectbox("Sexe de l'usager", options=["Masculin", "Féminin"])
+    equipement_securite = st.selectbox("Équipement de sécurité", options=["Non renseigné", "Aucun équipement", "Ceinture", "Casque", "Dispositif enfants", "Gilet réfléchissant", "Airbag (2RM/3RM)", "Gants (2RM/3RM)", "Gants + Airbag (2RM/3RM)", "Non déterminable", "Autre"])
+    age_victime = st.slider("Âge de la ou des victimes", min_value=0, max_value=100, step=1)
+    categorie_vehicule = st.selectbox("Catégorie du véhicule", options=["Bicyclette", "Cyclomoteur <50cm3", "Voiturette (Quadricycle à moteur carrossé)", "Référence inutilisée depuis 2006 (scooter immatriculé)", "Référence inutilisée depuis 2006 (motocyclette)", "Référence inutilisée depuis 2006 (side-car)", "VL seul", "Référence inutilisée depuis 2006 (VL + caravane)", "Référence inutilisée depuis 2006 (VL + remorque)", "VU seul 1,5T <= PTAC <= 3,5T avec ou sans remorque", "Référence inutilisée depuis 2006 (VU (10) + caravane)", "Référence inutilisée depuis 2006 (VU (10) + remorque)", "PL seul 3,5T <PTCA <= 7,5T", "PL seul > 7,5T", "PL > 3,5T + remorque", "Tracteur routier seul", "Tracteur routier + semi-remorque", "Référence inutilisée depuis 2006 (transport en commun)", "Référence inutilisée depuis 2006 (tramway)", "Engin spécial", "Tracteur agricole", "Scooter < 50 cm3", "Motocyclette > 50 cm3 et <= 125 cm3", "Scooter > 50 cm3 et <= 125 cm3", "Motocyclette > 125 cm3", "Scooter > 125 cm3", "Quad léger <= 50 cm3 (Quadricycle à moteur non carrossé)", "Quad lourd > 50 cm3 (Quadricycle à moteur non carrossé)", "Autobus", "Autocar", "Train", "Tramway", "3RM <= 50 cm3", "3RM > 50 cm3 <= 125 cm3", "3RM > 125 cm3", "EDP à moteur", "EDP sans moteur", "VAE", "Autre véhicule"])
+    obstacle_mobile = st.selectbox("Obstacle mobile heurté", options=["Non renseigné", "Aucun", "Piéton", "Véhicule", "Véhicule sur rail", "Animal domestique", "Animal sauvage", "Autre"])
+    type_motorisation = st.selectbox("Type de motorisation du véhicule", options=["Non renseigné", "Inconnue", "Hydrocarbures", "Hybride électrique", "Electrique", "Hydrogène", "Humaine", "Autre"])
+    categorie_route = st.selectbox("Catégorie de route", options=["Autoroute", "Route nationale", "Route Départementale", "Voie Communales", "Hors réseau public", "Parc de stationnement ouvert à la circulation publique", "Routes de métropole urbaine", "Autre"])
+    regime_circulation = st.selectbox("Régime de circulation", options=["Non renseigné", "A sens unique", "Bidirectionnelle", "A chaussées séparées", "Avec voies d’affectation variable"])
+    etat_surface = st.selectbox("État de la surface", options=["Non renseigné", "Normale", "Mouillée", "Flaques", "Inondée", "Enneigée", "Boue", "Verglacée", "Corps gras – huile", "Autre"])
+    situation_accident = st.selectbox("Situation de l’accident", options=["Non renseigné", "Aucun", "Sur chaussée", "Sur bande d’arrêt d’urgence", "Sur accotement", "Sur trottoir", "Sur piste cyclable", "Sur autre voie spéciale", "Autres"])
+    vitesse_max_autorisee = st.slider("Vitesse maximale autorisée", min_value=0, max_value=300, step=1)
+    lumiere = st.selectbox("Lumière", options=["Plein jour", "Crépuscule ou aube", "Nuit sans éclairage public", "Nuit avec éclairage public non allumé", "Nuit avec éclairage public allumé"])
+    commune = st.text_input("Commune (Code INSEE)")
+    localisation = st.selectbox("Localisation", options=["Hors agglomération", "En agglomération"])
+    intersection = st.selectbox("Intersection", options=["Hors intersection", "Intersection en X", "Intersection en T", "Intersection en Y", "Intersection à plus de 4 branches", "Giratoire", "Place", "Passage à niveau", "Autre intersection"])
+    conditions_atmospheriques = st.selectbox("Conditions atmosphériques", options=["Non renseigné", "Normale", "Pluie légère", "Pluie forte", "Neige - grêle", "Brouillard - fumée", "Vent fort - tempête", "Temps éblouissant", "Temps couvert", "Autre"])
+    type_collision = st.selectbox("Type de collision", options=["Non renseigné", "Deux véhicules - frontale", "Deux véhicules – par l’arrière", "Deux véhicules – par le coté", "Trois véhicules et plus – en chaîne", "Trois véhicules et plus - collisions multiples", "Autre collision", "Sans collision"])
+    latitude = st.slider("Latitude", min_value=-90.0, max_value=90.0, step=0.001)
+    longitude = st.slider("Longitude", min_value=-180.0, max_value=180.0, step=0.001)
 
     st.write("")  # Ajouter de l'espace vertical pour créer une nouvelle ligne
     if st.button("Valider"):
         # Effectuer le traitement ici
         pass
-
 
 def show_graph():
     st.markdown("<h1 id='graph' style='text-align: center;'>Graphique de Prédiction</h1>", unsafe_allow_html=True)
