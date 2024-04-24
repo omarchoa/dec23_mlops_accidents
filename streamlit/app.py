dfc85c94d19ac62bb7490e4630f1d118821b13ab
miklderoche <127089400+miklderoche@users.noreply.github.com>
2024-04-24 17:28:14 +0200

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -46,11 +46,6 @@ def authenticate_user(username, password):
 # Page de connexion
 def login_page():
     st.header("Connexion")
-    
-    col1, col2, col3 = st.columns([1, 3, 1])
-    with col2:
-        st.image("/mount/src/dec23_mlops_accidents/streamlit/images/bouclier.png", width=200)
-    st.write("") 
     st.markdown("<h1 style='text-align:center;'>SHIELD</h1><h6 style='text-align:center;'><em>Safety Hazard Identification and Emergency Law Deployment</em></h6>", unsafe_allow_html=True)
     
     # Champs de saisie pour le nom d'utilisateur et le mot de passe
