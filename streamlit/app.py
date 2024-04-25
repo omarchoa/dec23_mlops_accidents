7d9a9eb5914c5761c6709eb453119fd60dc93173
Omar Choa <omar.choa@gmail.com>
2024-04-25 19:50:47 +0200

    feat: get prediction & `request_id` from backend

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,5 +1,3 @@
-import os
-
 import requests
 
 import streamlit as st
@@ -62,11 +60,18 @@ def show_login_page():
     password = st.text_input("Mot de passe", type="password")
     if st.button("Se connecter"):
         if authenticate(username, password):
+            ## À partir des identifiants, créer la chaîne d'authentification à passer à la passerelle `gateway`
+            authentication_string = {"identification": f"{username}:{password}"}
+
+            ## La sauvegarder avec d'autres variables dans l'état de session pour une utilisation ultérieure dans d'autres fonctions
+            st.session_state["authentication_string"] = authentication_string
             st.session_state["authenticated"] = True
             st.session_state["username"] = username
-            # Effacer le contenu de la page
+
+            ## Effacer le contenu de la page
             st.empty()
-            # Forcer le rerun de la page pour afficher le contenu authentifié
+
+            ## Forcer le rerun de la page pour afficher le contenu authentifié
             st.experimental_rerun()
 
 
@@ -159,11 +164,192 @@ def show_features():
         unsafe_allow_html=True,
     )
 
-    # Caractéristiques
+    # Caractéristiques de l'accident
+
+    ## Correspondances
+    dict_catu = {
+        "Conducteur": 1,
+        "Passager": 2,
+        "Piéton": 3,
+    }
+    dict_sexe = {
+        "Masculin": 1,
+        "Féminin": 2,
+    }
+    dict_secu1 = {
+        "Non renseigné": -1,
+        "Aucun équipement": 0,
+        "Ceinture": 1,
+        "Casque": 2,
+        "Dispositif enfants": 3,
+        "Gilet réfléchissant": 4,
+        "Airbag (2RM/3RM)": 5,
+        "Gants (2RM/3RM)": 6,
+        "Gants + Airbag (2RM/3RM)": 7,
+        "Non déterminable": 8,
+        "Autre": 9,
+    }
+    dict_catv = {
+        "Indeterminable": 0,
+        "Bicyclette": 1,
+        "Cyclomoteur <50cm3": 2,
+        "Voiturette (Quadricycle à moteur carrossé)": 3,
+        "Référence inutilisée depuis 2006 (scooter immatriculé)": 4,
+        "Référence inutilisée depuis 2006 (motocyclette)": 5,
+        "Référence inutilisée depuis 2006 (side-car)": 6,
+        "VL seul": 7,
+        "Référence inutilisée depuis 2006 (VL + caravane)": 8,
+        "Référence inutilisée depuis 2006 (VL + remorque)": 9,
+        "VU seul 1,5T <= PTAC <= 3,5T avec ou sans remorque": 10,
+        "Référence inutilisée depuis 2006 (VU (10) + caravane)": 11,
+        "Référence inutilisée depuis 2006 (VU (10) + remorque)": 12,
+        "PL seul 3,5T <PTCA <= 7,5T": 13,
+        "PL seul > 7,5T": 14,
+        "PL > 3,5T + remorque": 15,
+        "Tracteur routier seul": 16,
+        "Tracteur routier + semi-remorque": 17,
+        "Référence inutilisée depuis 2006 (transport en commun)": 18,
+        "Référence inutilisée depuis 2006 (tramway)": 19,
+        "Engin spécial": 20,
+        "Tracteur agricole": 21,
+        "Scooter < 50 cm3": 30,
+        "Motocyclette > 50 cm3 et <= 125 cm3": 31,
+        "Scooter > 50 cm3 et <= 125 cm3": 32,
+        "Motocyclette > 125 cm3": 33,
+        "Scooter > 125 cm3": 34,
+        "Quad léger <= 50 cm3 (Quadricycle à moteur non carrossé)": 35,
+        "Quad lourd > 50 cm3 (Quadricycle à moteur non carrossé)": 36,
+        "Autobus": 37,
+        "Autocar": 38,
+        "Train": 39,
+        "Tramway": 40,
+        "3RM <= 50 cm3": 41,
+        "3RM > 50 cm3 <= 125 cm3": 42,
+        "3RM > 125 cm3": 43,
+        "EDP à moteur": 50,
+        "EDP sans moteur": 60,
+        "VAE": 80,
+        "Autre véhicule": 99,
+    }
+    dict_obsm = {
+        "Non renseigné": -1,
+        "Aucun": 0,
+        "Piéton": 1,
+        "Véhicule": 2,
+        "Véhicule sur rail": 4,
+        "Animal domestique": 5,
+        "Animal sauvage": 6,
+        "Autre": 9,
+    }
+    dict_motor = {
+        "Non renseigné": -1,
+        "Inconnue": 0,
+        "Hydrocarbures": 1,
+        "Hybride électrique": 2,
+        "Electrique": 3,
+        "Hydrogène": 4,
+        "Humaine": 5,
+        "Autre": 6,
+    }
+    dict_catr = {
+        "Autoroute": 1,
+        "Route nationale": 2,
+        "Route Départementale": 3,
+        "Voie Communales": 4,
+        "Hors réseau public": 5,
+        "Parc de stationnement ouvert à la circulation publique": 6,
+        "Routes de métropole urbaine": 7,
+        "Autre": 9,
+    }
+    dict_circ = {
+        "Non renseigné": -1,
+        "A sens unique": 1,
+        "Bidirectionnelle": 2,
+        "A chaussées séparées": 3,
+        "Avec voies d’affectation variable": 4,
+    }
+    dict_surf = {
+        "Non renseigné": -1,
+        "Normale": 1,
+        "Mouillée": 2,
+        "Flaques": 3,
+        "Inondée": 4,
+        "Enneigée": 5,
+        "Boue": 6,
+        "Verglacée": 7,
+        "Corps gras – huile": 8,
+        "Autre": 9,
+    }
+    dict_situ = {
+        "Non renseigné": -1,
+        "Aucun": 0,
+        "Sur chaussée": 1,
+        "Sur bande d’arrêt d’urgence": 2,
+        "Sur accotement": 3,
+        "Sur trottoir": 4,
+        "Sur piste cyclable": 5,
+        "Sur autre voie spéciale": 6,
+        "Autres": 8,
+    }
+    dict_lum = {
+        "Plein jour": 1,
+        "Crépuscule ou aube": 2,
+        "Nuit sans éclairage public": 3,
+        "Nuit avec éclairage public non allumé": 4,
+        "Nuit avec éclairage public allumé": 5,
+    }
+    dict_agg_ = {
+        "Hors agglomération": 1,
+        "En agglomération": 2,
+    }
+    dict_inter = {
+        "Hors intersection": 1,
+        "Intersection en X": 2,
+        "Intersection en T": 3,
+        "Intersection en Y": 4,
+        "Intersection à plus de 4 branches": 5,
+        "Giratoire": 6,
+        "Place": 7,
+        "Passage à niveau": 8,
+        "Autre intersection": 9,
+    }
+    dict_atm = {
+        "Non renseigné": -1,
+        "Normale": 1,
+        "Pluie légère": 2,
+        "Pluie forte": 3,
+        "Neige - grêle": 4,
+        "Brouillard - fumée": 5,
+        "Vent fort - tempête": 6,
+        "Temps éblouissant": 7,
+        "Temps couvert": 8,
+        "Autre": 9,
+    }
+    dict_col = {
+        "Non renseigné": -1,
+        "Deux véhicules - frontale": 1,
+        "Deux véhicules – par l’arrière": 2,
+        "Deux véhicules – par le coté": 3,
+        "Trois véhicules et plus – en chaîne": 4,
+        "Trois véhicules et plus - collisions multiples": 5,
+        "Autre collision": 6,
+        "Sans collision": 7,
+    }
+
+    ## Saisie
     jour_accident = st.date_input(
-        "Jour de l'accident", value=None, min_value=None, max_value=None, key=None
+        "Jour de l'accident",
+        value=None,
+        min_value=None,
+        max_value=None,
+        key=None,
     )
-    heure_accident = st.slider("Heure de l'accident", min_value=0, max_value=23, step=1)
+    heure_accident = st.slider(
+        "Heure de l'accident",
+        min_value=0,
+        max_value=23,
+        step=1,
+    )
     departement = st.text_input("Département (Code INSEE)")
     place_occupée = st.slider(
         "Place occupée dans le véhicule",
@@ -172,238 +358,147 @@ def show_features():
         step=1,
         help="10 – Piéton (non applicable)",
     )
-    nombre_victimes = st.slider("Nombre de victimes", min_value=0, max_value=25, step=1)
+    nombre_victimes = st.slider(
+        "Nombre de victimes",
+        min_value=0,
+        max_value=25,
+        step=1,
+    )
     nombre_vehicules = st.text_input("Nombre de véhicules impliqués")
     categorie_usager = st.selectbox(
-        "Catégorie d'usager", options=["Conducteur", "Passager", "Piéton"]
+        "Catégorie d'usager",
+        options=dict_catu.keys(),
     )
-    sexe_usager = st.selectbox("Sexe de l'usager", options=["Masculin", "Féminin"])
+    sexe_usager = st.selectbox(
+        "Sexe de l'usager",
+        options=dict_sexe.keys(),
+    )
     equipement_securite = st.selectbox(
         "Équipement de sécurité",
-        options=[
-            "Non renseigné",
-            "Aucun équipement",
-            "Ceinture",
-            "Casque",
-            "Dispositif enfants",
-            "Gilet réfléchissant",
-            "Airbag (2RM/3RM)",
-            "Gants (2RM/3RM)",
-            "Gants + Airbag (2RM/3RM)",
-            "Non déterminable",
-            "Autre",
-        ],
+        options=dict_secu1.keys(),
     )
     age_victime = st.slider(
-        "Âge de la ou des victimes", min_value=0, max_value=100, step=1
+        "Âge de la ou des victimes",
+        min_value=0,
+        max_value=100,
+        step=1,
     )
     categorie_vehicule = st.selectbox(
         "Catégorie du véhicule",
-        options=[
-            "Bicyclette",
-            "Cyclomoteur <50cm3",
-            "Voiturette (Quadricycle à moteur carrossé)",
-            "Référence inutilisée depuis 2006 (scooter immatriculé)",
-            "Référence inutilisée depuis 2006 (motocyclette)",
-            "Référence inutilisée depuis 2006 (side-car)",
-            "VL seul",
-            "Référence inutilisée depuis 2006 (VL + caravane)",
-            "Référence inutilisée depuis 2006 (VL + remorque)",
-            "VU seul 1,5T <= PTAC <= 3,5T avec ou sans remorque",
-            "Référence inutilisée depuis 2006 (VU (10) + caravane)",
-            "Référence inutilisée depuis 2006 (VU (10) + remorque)",
-            "PL seul 3,5T <PTCA <= 7,5T",
-            "PL seul > 7,5T",
-            "PL > 3,5T + remorque",
-            "Tracteur routier seul",
-            "Tracteur routier + semi-remorque",
-            "Référence inutilisée depuis 2006 (transport en commun)",
-            "Référence inutilisée depuis 2006 (tramway)",
-            "Engin spécial",
-            "Tracteur agricole",
-            "Scooter < 50 cm3",
-            "Motocyclette > 50 cm3 et <= 125 cm3",
-            "Scooter > 50 cm3 et <= 125 cm3",
-            "Motocyclette > 125 cm3",
-            "Scooter > 125 cm3",
-            "Quad léger <= 50 cm3 (Quadricycle à moteur non carrossé)",
-            "Quad lourd > 50 cm3 (Quadricycle à moteur non carrossé)",
-            "Autobus",
-            "Autocar",
-            "Train",
-            "Tramway",
-            "3RM <= 50 cm3",
-            "3RM > 50 cm3 <= 125 cm3",
-            "3RM > 125 cm3",
-            "EDP à moteur",
-            "EDP sans moteur",
-            "VAE",
-            "Autre véhicule",
-        ],
+        options=dict_catv.keys(),
     )
     obstacle_mobile = st.selectbox(
         "Obstacle mobile heurté",
-        options=[
-            "Non renseigné",
-            "Aucun",
-            "Piéton",
-            "Véhicule",
-            "Véhicule sur rail",
-            "Animal domestique",
-            "Animal sauvage",
-            "Autre",
-        ],
+        options=dict_obsm.keys(),
     )
     type_motorisation = st.selectbox(
         "Type de motorisation du véhicule",
-        options=[
-            "Non renseigné",
-            "Inconnue",
-            "Hydrocarbures",
-            "Hybride électrique",
-            "Electrique",
-            "Hydrogène",
-            "Humaine",
-            "Autre",
-        ],
+        options=dict_motor.keys(),
     )
     categorie_route = st.selectbox(
         "Catégorie de route",
-        options=[
-            "Autoroute",
-            "Route nationale",
-            "Route Départementale",
-            "Voie Communales",
-            "Hors réseau public",
-            "Parc de stationnement ouvert à la circulation publique",
-            "Routes de métropole urbaine",
-            "Autre",
-        ],
+        options=dict_catr.keys(),
     )
     regime_circulation = st.selectbox(
         "Régime de circulation",
-        options=[
-            "Non renseigné",
-            "A sens unique",
-            "Bidirectionnelle",
-            "A chaussées séparées",
-            "Avec voies d’affectation variable",
-        ],
+        options=dict_circ.keys(),
     )
     etat_surface = st.selectbox(
         "État de la surface",
-        options=[
-            "Non renseigné",
-            "Normale",
-            "Mouillée",
-            "Flaques",
-            "Inondée",
-            "Enneigée",
-            "Boue",
-            "Verglacée",
-            "Corps gras – huile",
-            "Autre",
-        ],
+        options=dict_surf.keys(),
     )
     situation_accident = st.selectbox(
         "Situation de l’accident",
-        options=[
-            "Non renseigné",
-            "Aucun",
-            "Sur chaussée",
-            "Sur bande d’arrêt d’urgence",
-            "Sur accotement",
-            "Sur trottoir",
-            "Sur piste cyclable",
-            "Sur autre voie spéciale",
-            "Autres",
-        ],
+        options=dict_situ.keys(),
     )
     vitesse_max_autorisee = st.slider(
-        "Vitesse maximale autorisée", min_value=0, max_value=300, step=1
+        "Vitesse maximale autorisée",
+        min_value=0,
+        max_value=300,
+        step=1,
     )
     lumiere = st.selectbox(
         "Lumière",
-        options=[
-            "Plein jour",
-            "Crépuscule ou aube",
-            "Nuit sans éclairage public",
-            "Nuit avec éclairage public non allumé",
-            "Nuit avec éclairage public allumé",
-        ],
+        options=dict_lum.keys(),
     )
     commune = st.text_input("Commune (Code INSEE)")
     localisation = st.selectbox(
-        "Localisation", options=["Hors agglomération", "En agglomération"]
+        "Localisation",
+        options=dict_agg_.keys(),
     )
     intersection = st.selectbox(
         "Intersection",
-        options=[
-            "Hors intersection",
-            "Intersection en X",
-            "Intersection en T",
-            "Intersection en Y",
-            "Intersection à plus de 4 branches",
-            "Giratoire",
-            "Place",
-            "Passage à niveau",
-            "Autre intersection",
-        ],
+        options=dict_inter.keys(),
     )
     conditions_atmospheriques = st.selectbox(
         "Conditions atmosphériques",
-        options=[
-            "Non renseigné",
-            "Normale",
-            "Pluie légère",
-            "Pluie forte",
-            "Neige - grêle",
-            "Brouillard - fumée",
-            "Vent fort - tempête",
-            "Temps éblouissant",
-            "Temps couvert",
-            "Autre",
-        ],
+        options=dict_atm.keys(),
     )
     type_collision = st.selectbox(
         "Type de collision",
-        options=[
-            "Non renseigné",
-            "Deux véhicules - frontale",
-            "Deux véhicules – par l’arrière",
-            "Deux véhicules – par le coté",
-            "Trois véhicules et plus – en chaîne",
-            "Trois véhicules et plus - collisions multiples",
-            "Autre collision",
-            "Sans collision",
-        ],
+        options=dict_col.keys(),
     )
-    latitude = st.slider("Latitude", min_value=-90.0, max_value=90.0, step=0.001)
-    longitude = st.slider("Longitude", min_value=-180.0, max_value=180.0, step=0.001)
+    latitude = st.slider(
+        "Latitude",
+        min_value=-90.0,
+        max_value=90.0,
+        step=0.001,
+    )
+    longitude = st.slider(
+        "Longitude",
+        min_value=-180.0,
+        max_value=180.0,
+        step=0.001,
+    )
+
+    ## Conversion
+    input_data_pred_call = {
+        "place": int(place_occupée),
+        "catu": int(dict_catu[categorie_usager]),
+        "sexe": int(dict_sexe[sexe_usager]),
+        "secu1": float(dict_secu1[equipement_securite]),
+        "year_acc": int(jour_accident.year),
+        "victim_age": int(age_victime),
+        "catv": int(dict_catv[categorie_vehicule]),
+        "obsm": int(dict_obsm[obstacle_mobile]),
+        "motor": int(dict_motor[type_motorisation]),
+        "catr": int(dict_catr[categorie_route]),
+        "circ": int(dict_circ[regime_circulation]),
+        "surf": int(dict_surf[etat_surface]),
+        "situ": int(dict_situ[situation_accident]),
+        "vma": int(vitesse_max_autorisee),
+        "jour": int(jour_accident.day),
+        "mois": int(jour_accident.month),
+        "lum": int(dict_lum[lumiere]),
+        "dep": int(departement),
+        "com": int(commune),
+        "agg_": int(dict_agg_[localisation]),
+        "inter": int(dict_inter[intersection]),
+        "atm": int(dict_atm[conditions_atmospheriques]),
+        "col": int(dict_col[type_collision]),
+        "lat": float(latitude),
+        "long": float(longitude),
+        "hour": int(heure_accident),
+        "nb_victim": int(nombre_victimes),
+        "nb_vehicules": int(nombre_vehicules),
+    }
 
     st.write("")
+
+    ## Prédiction
     if st.button("Valider"):
-        gravity = determine_gravity(
-            jour_accident, heure_accident, nombre_victimes, age_victime
+        ### Récupérer la chaîne d'authentification à partir de l'état de session
+        authentication_string = st.session_state["authentication_string"]
+
+        ### Appeler le microservice `prediction` en lui passant les données d'entrée et la chaîne d'authentification
+        response = requests.post(
+            url="http://gateway:8001/prediction/call",
+            json=input_data_pred_call,
+            headers=authentication_string,
         )
-        gravity_text = translate_gravity(gravity)
-        st.write(f"Gravité de l'accident : {gravity_text}")
 
-
-def determine_gravity(jour_accident, heure_accident, nombre_victimes, age_victime):
-    if nombre_victimes > 5:
-        return 1  # Grave
-    else:
-        return 0  # Non grave
-
-
-def translate_gravity(gravity):
-    if gravity == 0:
-        return "Non Grave"
-    elif gravity == 1:
-        return "Grave"
-    else:
-        return "Non déterminé"
+        ### Afficher la réponse du microservice `prediction`
+        st.write(response.text)
 
 
 def show_feedback_features():
