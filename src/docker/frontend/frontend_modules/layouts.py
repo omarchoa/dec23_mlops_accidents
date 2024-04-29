45ed5e2640369acf9479d4972a10af1ae7a19be5
Omar Choa <omar.choa@gmail.com>
2024-04-29 21:29:47 +0200

    refactor: prepare ui layout for admin

--- src/docker/frontend/frontend_modules/layouts.py
+++ src/docker/frontend/frontend_modules/layouts.py
@@ -6,59 +6,59 @@ from frontend_modules.navigation import button_actions
 # define admin function
 def admin():
 
-    # # set home page as landing page
-    # if "page" not in st.session_state:
-    #     st.session_state["page"] = "home"
+    ## set home page as landing page
+    if "page" not in st.session_state:
+        st.session_state["page"] = "home"
 
     ## display home section
     st.sidebar.header(body="Accueil")
-    button_home = st.sidebar.button(label="Accueil")
-    button_status = st.sidebar.button(label="État du système")
-    button_logout = st.sidebar.button(label="Déconnexion")
+    if st.sidebar.button(label="Accueil"):
+        st.session_state["page"] = "home"
+    if st.sidebar.button(label="État du système"):
+        st.session_state["page"] = "status"
+    if st.sidebar.button(label="Déconnexion"):
+        st.session_state["page"] = "logout"
 
     ## display users section
     st.sidebar.header(body="Utilisateurs")
-    st.sidebar.button(
-        label="Visualiser tous les utilisateurs", key="users_all"
-    )  ### for debugging purposes only
-    st.sidebar.button(label="Ajouter un utilisateur", key="users_register")
-    st.sidebar.button(label="Supprimer un utilisateur", key="users_remove")
+    if st.sidebar.button(
+        label="Visualiser tous les utilisateurs"
+    ):  ### for debugging purposes only
+        st.session_state["page"] = "users_all"
+    if st.sidebar.button(label="Ajouter un utilisateur"):
+        st.session_state["page"] = "users_register"
+    if st.sidebar.button(label="Supprimer un utilisateur"):
+        st.session_state["page"] = "users_remove"
 
     ## display data section
     st.sidebar.header(body="Données")
-    st.sidebar.button(
-        label="Télécharger et préparer des données", key="data_download_prep_run"
-    )
+    if st.sidebar.button(label="Télécharger et préparer des données"):
+        st.session_state["page"] = "data_download_prep_run"
 
     ## display modeling section
     st.sidebar.header(body="Modélisation")
-    st.sidebar.button(label="Entraîner le modèle", key="training_train")
+    if st.sidebar.button(label="Entraîner le modèle"):
+        st.session_state["page"] = "training_train"
 
     ## display prediction section
     st.sidebar.header(body="Prédiction")
-    st.sidebar.button(label="Effectuer une prédiction test", key="prediction_test")
-    st.sidebar.button(label="Effectuer une prédiction réelle", key="prediction_call")
+    if st.sidebar.button(label="Effectuer une prédiction test"):
+        st.session_state["page"] = "prediction_test"
+    if st.sidebar.button(label="Effectuer une prédiction réelle"):
+        st.session_state["page"] = "prediction_call"
 
     ## display scoring section
     st.sidebar.header(body="Évaluation")
-    st.sidebar.button(
-        label="Valider ou corriger une prédiction",
-        key="scoring_label_prediction",
-    )
-    st.sidebar.button(label="Mettre à jour le F1 score", key="scoring_update_f1_score")
-    st.sidebar.button(
-        label="Récupérer le dernier F1 score", key="scoring_get_latest_f1_score"
-    )
-    st.sidebar.button(label="Récupérer tous les F1 scores", key="scoring_get_f1_scores")
-    st.sidebar.button(
-        label="Visualiser l'évolution des F1 scores",
-        key="scoring_plot_f1_scores",
-    )
-
-    ## save button actions to session state
-    st.session_state["button_home"] = button_home
-    st.session_state["button_status"] = button_status
-    st.session_state["button_logout"] = button_logout
+    if st.sidebar.button(label="Valider ou corriger une prédiction"):
+        st.session_state["page"] = "scoring_label_prediction"
+    if st.sidebar.button(label="Mettre à jour le F1 score"):
+        st.session_state["page"] = "scoring_update_f1_score"
+    if st.sidebar.button(label="Récupérer le dernier F1 score"):
+        st.session_state["page"] = "scoring_get_latest_f1_score"
+    if st.sidebar.button(label="Récupérer tous les F1 scores"):
+        st.session_state["page"] = "scoring_get_f1_scores"
+    if st.sidebar.button(label="Visualiser l'évolution des F1 scores"):
+        st.session_state["page"] = "scoring_plot_f1_scores"
 
     ## execute button actions
     button_actions()
