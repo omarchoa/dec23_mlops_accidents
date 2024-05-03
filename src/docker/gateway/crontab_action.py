1923b5e703838b7c1eff8cb0453402ef586f5dbf
Omar Choa <omar.choa@gmail.com>
2024-05-03 19:23:03 +0200

    feat: improve `crontab_action.py`
    - set threshold to 0.74 (value in cahier des charges)
    - replace `/training/train` with `/training/retrain`
    - update f1 score automatically after retraining
    - apply automatic pep8 formatting with black

--- src/docker/gateway/crontab_action.py
+++ src/docker/gateway/crontab_action.py
@@ -1,15 +1,15 @@
 import datetime
-import os
+
 import requests
 
-threshold = 0.75
+threshold = 0.74
 URI = "http://0.0.0.0:8001"  # gateway
 log_filename = "/logs/crontab.csv"
 retraining = False
 
 
 def format_timestamp(timestamp):
-    return timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    return timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
 
 def log(start, comment):
@@ -26,24 +26,32 @@ response = requests.get(
 if response.status_code == 200:
     latest_score = eval(response.content.decode("utf-8"))
     if latest_score >= threshold:
-        comment = f"latest_score ({latest_score}) >= threshold ({threshold}): nothing to do"
+        comment = (
+            f"latest_score ({latest_score}) >= threshold ({threshold}): nothing to do"
+        )
     else:
         comment = f"latest_score ({latest_score}) < threshold ({threshold}): model shall be retrained !"
         retraining = True
 else:
-    comment = f"request status on {URI}/scoring/get-latest-f1-score: {response.status_code}"
+    comment = (
+        f"request status on {URI}/scoring/get-latest-f1-score: {response.status_code}"
+    )
 
 log(start, comment)
 
 if retraining:
     start = datetime.datetime.now()
     response = requests.get(
-        url=f"{URI}/training/train",
+        url=f"{URI}/training/retrain",
+        headers={"identification": "robot:Autom@t"},
+    )
+    requests.get(
+        url=f"{URI}/scoring/update-f1-score",
         headers={"identification": "robot:Autom@t"},
     )
     if response.status_code == 200:
         end = datetime.datetime.now()
         comment = f"retraining took {end - start} s"
     else:
-        comment = f"retraining failed, request status on {URI}/training/train: {response.status_code}"
+        comment = f"retraining failed, request status on {URI}/training/retrain: {response.status_code}"
     log(start, comment)
